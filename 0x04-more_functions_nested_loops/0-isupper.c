48f3def835b827abb06f4b84425f6a730eae66fd
BenjaminEchez <echebenjamin@yahoo.com>
2022-09-16 07:02:20 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,9 +8,13 @@
  *
  * Return: 1 if uppercase, other case 0.
  */
-int _isupper(int c) 
+
+int _isupper(int c)
 {
-  if (c >= 65 && c <= 90)
-    return (1);
-  return (0); 
+  if ((c >= 'A') && (c <= 'Z'))
+    {
+      return (1);
+    }
+
+  return (0);
 }
